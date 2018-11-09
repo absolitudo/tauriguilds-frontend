85b694b97e84474dec0fb17210cd1e8a8c94d5f1
Ákos <absolitudo@gmail.com>
2018-11-09 15:33:15 +0100

    basic routing

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 function App() {
-    return <div>app</div>;
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={SayApp} />
+                <Route exact path="/guild/:guildName" component={SayGuild} />
+                <Route component={Say404} />
+            </Switch>
+        </Router>
+    );
+}
+
+function SayApp() {
+    return (
+        <div>
+            app <Link to="./guild/a guild"> linking </Link>{" "}
+        </div>
+    );
+}
+
+function SayGuild({ match }) {
+    return <div>{match.params.guildName}</div>;
+}
+
+function Say404() {
+    return <div>404</div>;
 }
 
 export default App;
