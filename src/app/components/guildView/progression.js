6313827580d893badb15b0fd62d1b5fee4d7a709
Ákos <absolitudo@gmail.com>
2018-12-01 05:23:28 +0100

    new look for progression component, include progression component styles

--- src/app/components/guildView/progression.js
+++ src/app/components/guildView/progression.js
@@ -1,4 +1,10 @@
 import React from "react";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Collapse from "@material-ui/core/Collapse";
+import ExpandLess from "@material-ui/icons/ExpandLess";
+import ExpandMore from "@material-ui/icons/ExpandMore";
 
 import { mapInstanceToPicture } from "./helpers";
 
@@ -19,21 +25,77 @@ function Progression(props) {
     );
 }
 
-function Instance(props) {
-    let picture = mapInstanceToPicture(props.instance.instanceName);
+class Instance extends React.PureComponent {
+    constructor(props) {
+        super(props);
 
-    return (
-        <div className="progression-instance-container">
-            <div
-                className="progression-instance"
-                style={{
-                    background: "url(" + picture + ")"
-                }}
-            >
-                {props.instance.abbreviation}
+        this.state = {
+            open: false
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState(state => ({ open: !state.open }));
+    }
+
+    render() {
+        let picture = mapInstanceToPicture(this.props.instance.instanceName);
+
+        let bosses = [];
+
+        for (let key in this.props.instance) {
+            if (key !== "abbreviation" && key !== "instanceName") {
+                bosses.push({
+                    bossName: key,
+                    defeated: this.props.instance[key]
+                });
+            }
+        }
+
+        return (
+            <div className="progression-instance-container">
+                <ListItem
+                    button
+                    onClick={this.handleClick}
+                    className="progression-instance"
+                    style={{ background: "url(" + picture + ")" }}
+                >
+                    <ListItemText primary={this.props.instance.abbreviation} />
+                    {this.state.open ? <ExpandLess /> : <ExpandMore />}
+                </ListItem>
+
+                <Collapse in={this.state.open} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                        {bosses.map(boss => {
+                            return (
+                                <ListItem className="instance-boss-container">
+                                    <ListItemText
+                                        primary={boss.bossName}
+                                        secondary={
+                                            <p
+                                                className={
+                                                    "boss-defeated " +
+                                                    (boss.defeated
+                                                        ? "green"
+                                                        : "red")
+                                                }
+                                            >
+                                                {boss.defeated
+                                                    ? "Defeated"
+                                                    : "Alive"}
+                                            </p>
+                                        }
+                                    />
+                                </ListItem>
+                            );
+                        })}
+                    </List>
+                </Collapse>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Progression;
