362096cb9109695e4b0080ab7f39f348b1f925f9
Ákos <absolitudo@gmail.com>
2018-12-08 14:30:51 +0100

    more error handling

--- src/app/components/handleError/index.js
+++ src/app/components/handleError/index.js
@@ -3,33 +3,42 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { withRouter } from "react-router-dom";
 
-import { removeError } from "../../redux/actions";
+import { setError, removeError } from "../../redux/actions";
 
 let listening = false;
 
-function HandleError({ error, children, history, removeError }) {
-    if (error) {
-        if (!listening) {
-            listening = true;
-            let stopListening = history.listen(() => {
-                stopListening();
-                listening = false;
-                removeError();
-            });
+class HandleError extends React.PureComponent {
+    componentDidCatch(err, info) {
+        if (typeof err !== "string") {
+            err = "Unexpected error";
         }
-        if (typeof error !== "string") {
-            error = "unexpected error occured";
-        }
-        return (
-            <section className="error">
-                <h1>Something went wrong :(</h1>
-                <p>{error}</p>
-                <p>Here is a cat for you</p>
-                <img src="https://loremflickr.com/280/280/cat" alt="cat" />
-            </section>
-        );
+        this.props.setError(err);
     }
-    return children;
+    render() {
+        let { error, children, history, removeError } = this.props;
+        if (error) {
+            if (!listening) {
+                listening = true;
+                let stopListening = history.listen(() => {
+                    stopListening();
+                    listening = false;
+                    removeError();
+                });
+            }
+            if (typeof error !== "string") {
+                error = "unexpected error occured";
+            }
+            return (
+                <section className="error">
+                    <h1>Something went wrong :(</h1>
+                    <p>{error}</p>
+                    <p>Here is a cat for you</p>
+                    <img src="https://loremflickr.com/280/280/cat" alt="cat" />
+                </section>
+            );
+        }
+        return children;
+    }
 }
 
 function mapStateToProps(state) {
@@ -39,7 +48,7 @@ function mapStateToProps(state) {
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators({ removeError }, dispatch);
+    return bindActionCreators({ setError, removeError }, dispatch);
 }
 
 export default connect(
